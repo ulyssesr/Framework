a9e310b971273929c83fee530de79c9c09b7c700
Ulysses <ulyssesr@yahoo.com>
2015-07-09 00:55:12 -0700

    move main menu to footer section

--- footer.php
+++ footer.php
@@ -10,6 +10,15 @@
 
 	</div><!-- #content -->
 
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<h1 class="menu-toggle"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/menu.png" alt="menu.png"/></h1>
+		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'framework' ); ?></a>
+
+		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
+
+	</nav><!-- #site-navigation -->
+
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<p>Copyright &copy; <?php echo '2003 - '. date("Y"); ?>. All rights reserved.</p>
--- header.php
+++ header.php
@@ -21,15 +21,6 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
-	<nav id="site-navigation" class="main-navigation" role="navigation">
-		<h1 class="menu-toggle"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/menu.png" alt="menu.png"/></h1>
-		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'framework' ); ?></a>
-
-		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
-
-	</nav><!-- #site-navigation -->
-
 	<header id="masthead" class="site-header" role="banner">
 
 		<div class="site-branding">
