d6337b67b7d7a5a63f187acece2f980ca6944fea
Ulysses <ulyssesr@yahoo.com>
2015-10-22 16:37:56 -0700

    update search page

--- search-page.php
+++ search-page.php
@@ -15,17 +15,15 @@ get_header(); ?>
 				</header>
 
 				<div class="entry-content">
-
-					<?php get_search_form(); ?>
-
-					<br/>
 					
 					<p><img src="http://ulyssesonline.com/wp-content/uploads/2014/12/A-Pirate-Ship-Open-Sail-to-the-Ocean-Coloring-Page.jpg" width="150"/></p>
 
 					<p>Arrr, matey, ye've come to the right place!<br/>
-					Type in a keyword on the Search form above.<br/> 
-					Or plunder the <a href="/archives/">Archives</a> for more hidden treasures.</p>
-								
+					Type in a keyword or two to search for your booty.<br/> 
+					Or you can plunder the <a href="/archives/">archives</a> for more hidden treasures.</p>
+					
+					<?php get_search_form(); ?>
+
 				</div>
 
 			</article>
