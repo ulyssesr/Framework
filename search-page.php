250d589763e57fca8c83a76d41bd91d283061de5
Ulysses <ulyssesr@yahoo.com>
2016-09-05 21:34:35 -0700

    updated search page

--- search-page.php
+++ search-page.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-				<header class="entry-header">			
+				<header class="entry-header">
 					<h1 class="entry-title">Search</h1>
 				</header>
 
@@ -19,14 +19,13 @@ get_header(); ?>
 
 					<p><img src="http://ulyssesonline.com/wp-content/uploads/2014/12/A-Pirate-Ship-Open-Sail-to-the-Ocean-Coloring-Page.jpg" width="150"/></p>
 
+					<p>Ahoy! ye've come to the right place!<br/>
+					   Arr matey, ye be searchin for some booty?<br/>
+					   Plunder our <a href="/archives/">archives</a> for hidden treasures.
+					</p>
+
 					<p><?php get_search_form(); ?></p>
 
-					<p>Ahoy!<br/>
-						 Arr matey, ye've come to the right place!<br/>
-					   Ye searchin for your next booty?<br/>
-					   Plunder our <a href="/archives/">archives</a> for more hidden treasures.
-					</p>
-			
 				</div>
 
 			</article>
